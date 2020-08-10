19c356b07ca921711d2513e2742dd3a6d364ef90
Oryam <48219965+or-yam@users.noreply.github.com>
2020-08-10 10:01:11 +0300

    added more riddles

--- src/App.js
+++ src/App.js
@@ -5,18 +5,60 @@ import Score from './components/Score.jsx';
 import Solution from './components/Solution.jsx';
 import EndGame from './components/EndGame.jsx';
 
+const riddles = [
+  {
+    word: 'mountain',
+    hint:
+      'What has roots as nobody sees, Is taller than trees, Up, up it goes,And yet never grows?',
+  },
+  {
+    word: 'Teeth',
+    hint:
+      'Thirty white horses on a red hill, First they champ, Then they stamp, Then they stand still.',
+  },
+  {
+    word: 'Wind',
+    hint:
+      'Voiceless it cries, Wingless flutters, Toothless bites, Mouthless mutters.',
+  },
+  {
+    word: 'Sun',
+    hint:
+      'An eye in a blue face Saw an eye in a green face. "That eye is like to this eye" Said the first eye,"But in low place,Not in high place."',
+  },
+  {
+    word: 'Dark',
+    hint:
+      'It cannot be seen, cannot be felt, Cannot be heard, cannot be smelt. It lies behind stars and under hills, And empty holes it fills. It comes first and follows after, Ends life, kills laughter.',
+  },
+  {
+    word: 'Egg',
+    hint:
+      'A box without hinges, key, or lid, Yet golden treasure inside is hid.',
+  },
+  {
+    word: 'Time',
+    hint:
+      'This thing all things devours: Birds, beasts, trees, flowers; Gnaws iron, bites steel; Grinds hard stones to meal; Slays king, ruins town, And beats high mountain down.',
+  },
+];
 class App extends Component {
   constructor() {
     super();
     this.state = {
       letterStatus: this.generateLettersStatus(),
       solution: {
-        word: 'mountain',
-        hint:
-          'What has roots as nobody sees,Is taller than treesUp, up it goes,And yet never grows?',
+        word: this.generateRiddle().word,
+        hint: this.generateRiddle().hint,
       },
       score: 100,
     };
+  }
+
+  generateRiddle() {
+    let rndNum = Math.floor(Math.random() * riddles.length);
+    let solution = riddles[rndNum];
+    return solution;
   }
 
   generateLettersStatus() {
@@ -46,7 +88,11 @@ class App extends Component {
   upDateScore = (operation) => {
     let newScore = this.state.score;
     operation === 'plus' ? (newScore += 10) : (newScore -= 10);
-    this.setState({ score: newScore });
+    if (newScore > 100) {
+      this.setState({ score: 100 });
+    } else {
+      this.setState({ score: newScore });
+    }
   };
 
   endGame = () => {
@@ -65,7 +111,7 @@ class App extends Component {
     const { letterStatus, solution, score } = this.state;
     return !this.endGame() ? (
       <div className="game-container">
-        <div className="stats-container">Stats</div>
+        <div className="stats-container"></div>
         <div className="score-container">
           {' '}
           <Score score={score} />
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -5,16 +5,22 @@ class Score extends Component {
     const { score } = this.props;
 
     return (
-      <div
-        className={
-          score >= 80
-            ? 'highScore'
-            : score < 80 && score >= 50
-            ? 'mediumScore'
-            : 'lowScore'
-        }
-      >
-        {score}
+      <div>
+        <div className="hangman">
+          <img src={`../../assets/${score}.png`} alt="" />
+        </div>
+
+        <div
+          className={
+            score >= 80
+              ? 'highScore'
+              : score < 80 && score >= 50
+              ? 'mediumScore'
+              : 'lowScore'
+          }
+        >
+          {score}
+        </div>
       </div>
     );
   }
